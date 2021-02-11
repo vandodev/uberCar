5916a3cdc1a003b21fcbd3511fc6b08e1256b12d
Evandro oliveira <vandoaparecido@hotmail.com>
2021-02-11 17:59:24 -0300

    Exibindo mapa

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 import {css} from './assets/css/Css';
 
@@ -7,9 +8,16 @@ export default function App() {
   return (
 
     <View style={css.container}>
-      <View style={css.map}>
-
-      </View>
+        <MapView
+              style={css.map}
+              region={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121,
+              }}
+    >
+      </MapView>
 
       <View style={css.search}>
 
